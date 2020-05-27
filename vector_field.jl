15076b1447ab39c178cafaf1174251a31e0feab7
Mikolaj Langner <mikilan123@gmail.com>
2020-05-27 22:22:36 +0200

    Full plotting fields in 2D and 3D, not parametrized yet

--- vector_field.jl
+++ vector_field.jl
@@ -5,27 +5,119 @@ using Makie
 using LinearAlgebra
 
 
+abstract type ColorVector end
+
+
+mutable struct ColorVector2D <: ColorVector
+
+    magnitude::Float64
+    direction::Point2f0
+    color::RGBf0
+    size::Float64
+
+    ColorVector2D(point::Array{T, 1}, decreaser::Real) where T <: Real = new(norm(point),
+                                                            Point2f0(normalize(point)/decreaser),
+                                                            RGBf0(0, 0, 0), 1.0)
+
+end
+
+
+mutable struct ColorVector3D <: ColorVector
+
+    magnitude::Float64
+    direction::Point3f0
+    color::RGBf0
+    size::Float64
+
+    ColorVector3D(point::Array{T, 1}, decreaser::Real) where T <: Real = new(norm(point),
+                                                        Point3f0(normalize(point)/decreaser),
+                                                        RGBf0(0, 0, 0), 1.0)
+
+end
+
+
+Base.isless(v::ColorVector, w::ColorVector) = v.magnitude < w.magnitude ? true : false
+Base.isequal(v::ColorVector, w::ColorVector) = v.magnitude == w.magnitude ? true : false
+
+
+function changeLength(v::ColorVector, longest::Real)
+
+    v.size = v.magnitude / longest
+    v.direction = v.direction * v.size
+    return v
+
+end
+
+
+function changeColor(v::ColorVector, longest::Real)
+
+    v.color = v.direction[end] >= 0 ? RGBf0(v.size, 1-v.size, 0) : RGBf0(0, 1-v.size, v.size)
+    return v
+
+end
+
+
+function differVectors(vectors::Array{T, 1})::Array{T, 1} where T <: ColorVector
+
+    vectors = sort(vectors)
+    vectors = changeLength.(vectors, vectors[end].magnitude)
+    vectors = changeColor.(vectors, vectors[end].magnitude)
+    return vectors
+
+end
+
+
 function field2D(f::Function, xbounds::Tuple{Real, Real}, ybounds::Tuple{Real, Real})
+
     points = [Point2f0(i, j) for i in LinRange(xbounds[1], xbounds[2], 20)
                              for j in LinRange(ybounds[1], ybounds[2], 20)]
+
     pointf(p::Point2f0) = f(p[1], p[2])
-    vectors = @. Point2f0(normalize(pointf(points))/20)
-    scene = arrows(points, vectors, arrowsize=0.02, linewidth=1, linecolor=:red)
+    vectors = @. ColorVector2D(pointf(points), 10)
+    vectors = differVectors(vectors)
+
+    scene = Scene()
+    for (point, vector) in zip(points, vectors)
+        arrows!([point], [vector.direction],
+                arrowsize=0.05vector.size, linewidth=2vector.size,
+                linecolor=vector.color, arrowcolor=vector.color)
+    end
+
     scene |> display
     return scene
+
 end
+
+
+# Example
+field2D((x,y)->[x^2+y^2,x-y^2],(-1,1),(-1,1))
 
 
 function field3D(f::Function, xbounds::Tuple{Real, Real}, ybounds::Tuple{Real, Real}, zbounds::Tuple{Real,Real})
+
     points = [Point3f0(i, j, k) for i in LinRange(xbounds[1], xbounds[2], 10)
-                             for j in LinRange(ybounds[1], ybounds[2], 10)
-                             for k in LinRange(zbounds[1], zbounds[2], 10)]
+                                for j in LinRange(ybounds[1], ybounds[2], 10)
+                                for k in LinRange(zbounds[1], zbounds[2], 10)]
+
     pointf(p::Point3f0) = f(p[1], p[2], p[3])
-    vectors = @. Point3f0(normalize(pointf(points))/10)
-    scene = arrows(points, vectors, arrowsize=0.02, linewidth=1, linecolor=:black)
+    vectors = @. ColorVector3D(pointf(points), 10)
+    vectors = differVectors(vectors)
+
+    scene = Scene()
+    for (point, vector) in zip(points, vectors)
+        arrows!([point], [vector.direction],
+                arrowsize=0.05vector.size, linewidth=2vector.size,
+                linecolor=vector.color, arrowcolor=vector.color)
+    end
+
     scene |> display
     return scene
+
 end
+
+
+# Example
+field3D((x,y,z)->[x^2+y^2,x+y,z],(-1,1),(-1,1),(-1,1))
 
 
 end
